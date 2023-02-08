96a39ed2acefdb08093eb3c24b195b471cf3bceb
Kip <103238779+kipperz@users.noreply.github.com>
2023-02-08 12:07:05 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord, logging, platform, os, sys
+from discord.ext import commands
+
+from config import secrets, settings
+
+bot = commands.Bot(
+    command_prefix = '!',
+    intents = discord.Intents.all(),
+    application_id = settings.application_id,
+    log_handler = logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
+)
+bot.remove_command('help')
+bot.synced = False
+
+@bot.event
+async def on_ready():
+    await bot.wait_until_ready()
+    print('---------------------------------------------------')
+    print(f'Logged in as {bot.user.name} | {bot.user.id}')
+    print(f"Discord.py API version: {discord.__version__}")
+    print(f"Python version: {platform.python_version()}")
+    print('---------------------------------------------------')
+
+if __name__ == "__main__":
+    for file in os.listdir('cogs/'):
+        if file.endswith('.py'):
+            bot.load_extension(f'cogs.{file[:-3]}')
+
+bot.run(secrets.bot_token)
